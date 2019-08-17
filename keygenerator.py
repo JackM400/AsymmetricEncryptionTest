1b8c2c091c3a7114f5ff7530bd31fd6d9b59b466
JackM400 <jack.millar400@gmail.com>
2019-08-17 17:09:25 +0100

    euler's algorithm and advanced euler's algorithm functions

--- keygenerator.py
+++ keygenerator.py
@@ -38,11 +38,34 @@ def initialise():
 
 
 def gcd(x, y):
-    return
+    while y != 0:
+        (x, y) = (y, x % y)
+    return x
 
 
-def gcdInverse(relCoPrime, phi):
-    return 0
+def gcdInverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+
+    while relCoPrime > 0:
+        temp1 = temp_phi / e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        relCoPrime = temp2
+
+        x = x2 - temp1 * x1
+        y = d - temp1 * y1
+
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+
+    if temp_phi == 1:
+        return d + phi
 
 
 def keypairGenerator(p, q):
